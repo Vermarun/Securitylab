23c632cb3922939470274807495b3f91dfef74b7
arun verma <arun@localadmin8.aapl.local>
2017-09-25 12:08:14 +0530

    Required comments

--- 1.Euclid_gcd.cpp
+++ 1.Euclid_gcd.cpp
@@ -1,21 +1,24 @@
 #include<iostream>
 using namespace std;
 
+
+// compute gcd of given numbers a & b
 int gcd(int a ,int b){
 
 	int r;
 	if (b==0){
-		return a;	
+		return a;
 	}
-	else 
+	else
 		{r = a % b;
-		
+
 	gcd(b,r);}
 }
 
 int main(){
-	
+
 	int a ,b;
+	// give the values for which gcd needs to be computed 
 	cout<<"\n enter the values of a & b (a>=b)";
 	cin>>a>>b;
 	 cout<<"\n gcd ="<<gcd(a,b);
